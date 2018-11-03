f3f76f555ee3e944db5d8c63b25a79e015bcfc27
EC2 Default User <ec2-user@ip-172-31-15-38.us-east-2.compute.internal>
2018-11-03 15:46:58 +0000

    Enforced login with TAMU mail id

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,6 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :google_oauth2, "463589924355-d9pqvmo0sg54tqrknb7lfrlrp18qllms.apps.googleusercontent.com", "vhR2Bmv-l5wY7IUvDCzsBIz_"
+  provider :google_oauth2, "463589924355-d9pqvmo0sg54tqrknb7lfrlrp18qllms.apps.googleusercontent.com", "vhR2Bmv-l5wY7IUvDCzsBIz_",
+           {
+               hd: 'tamu.edu'
+           }
 end
