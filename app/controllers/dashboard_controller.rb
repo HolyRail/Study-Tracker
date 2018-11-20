ffa89d3312d6717512f6725ba352ce65870269b3
EC2 Default User <ec2-user@ip-172-31-4-75.us-east-2.compute.internal>
2018-11-20 00:36:23 +0000

    New changes

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,5 +1,23 @@
 class DashboardController < ApplicationController
   def index
-    session.delete(:user_id)
+    @current_user ||= User.find_by_id(session[:user_id])
+    subjects = @current_user.subjects
+    @out_subjects = []
+    subjects.each do |subject|
+      out_subject = Hash.new
+      out_subject['name'] = subject.name
+      schedules = subject.schedules
+      study_session_count = 0
+      study_completed_count = 0
+      schedules.each do |schedule|
+        study_session_count += 1
+        if schedule.completed
+          study_completed_count += 1
+        end
+      end
+      print subject
+      out_subject['completion_rate'] = (study_completed_count/study_session_count)*100
+      @out_subjects.push(out_subject)
+    end
   end
 end
